8d002b047f534c95b769d668db37807c356a3d34
SaimaAmjad786 <160956110+SaimaAmjad786@users.noreply.github.com>
2025-01-04 02:18:08 -0800

    updated code

--- src/app/components/trending.tsx
+++ src/app/components/trending.tsx
@@ -83,113 +83,6 @@
 
 
 
-
-
-// "use client";
-// import React, { useEffect, useState } from "react";
-// import Image from "next/image";
-// import { client } from "@/sanity/lib/client";
-
-// const products = [
-//   {
-//     id: 1,
-//     name: "Cantilever chair",
-//     price: "$26.00",
-//     oldPrice: "$42.00",
-//     img: "/pink.png",
-//   },
-//   {
-//     id: 2,
-//     name: "Cantilever chair",
-//     price: "$26.00",
-//     oldPrice: "$42.00",
-//     img: "/reddish.png",
-//   },
-//   {
-//     id: 3,
-//     name: "Cantilever chair",
-//     price: "$42.00",
-//     oldPrice: "$90.00",
-//     img: "/white.png",
-//   },
-//   {
-//     id: 4,
-//     name: "Cantilever chair",
-//     price: "$42.00",
-//     oldPrice: "$80.00",
-//     img: "/brown.png",
-//   },
-// ];
-
-// interface TrendingProduct {
-//   cartHeading: string;
-//   cartImg: string;
-//   cartPrice: string;
-// }
-
-// const Trending = () => {
-//   const [trendingProducts, setTrendingProducts] = useState<TrendingProduct[]>([]);
-
-//   useEffect(() => {
-//     async function fetchTrendingProducts() {
-//       const res = await client.fetch(
-//         `*[_type == 'trendProduct'][0].sections[0].cart[]{'cartHeading':cartHeading, 'cartImg':cartImg.asset->url, 'cartPrice':cartPrice}`
-//       );
-//       setTrendingProducts(res);
-//     }
-
-//     fetchTrendingProducts();
-//   }, []);
-
-//   return (
-//     <section className="text-gray-600 body-font py-12">
-//       <div className="container mx-auto px-4">
-//         {/* Section Header */}
-//         <div className="flex flex-col text-center w-full mb-12">
-//           <h1 className="text-3xl font-bold text-gray-900">Trending Products</h1>
-//         </div>
-
-//         {/* Products Grid */}
-//         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-//           {trendingProducts.map((product, index) => (
-//             <div
-//               key={index}
-//               className="p-4 border border-gray-200 rounded-lg hover:shadow-lg transition-shadow duration-300 bg-gray-100"
-//             >
-//               {/* Product Image */}
-//               <Image
-//                 src={product.cartImg}
-//                 alt={product.cartHeading}
-//                 className="rounded-lg w-full h-60 object-cover mb-4"
-//                 width={500}
-//                 height={100}
-//               />
-//               {/* Product Name */}
-//               <h2 className="text-black font-medium text-lg mb-2">
-//                 {product.cartHeading}
-//               </h2>
-//               {/* Pricing */}
-//               <div className="text-[#151875] text-sm flex justify-center gap-2">
-//                 <span>{product.cartPrice}</span>
-//                 <span className="line-through text-black">
-//                   {products.oldPrice}
-//                 </span>
-//               </div>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Trending;
-
-
-
-
-
-
 import React from "react";
 import Image from "next/image";
 import { client } from "@/sanity/lib/client";
@@ -268,10 +161,7 @@ const [cartHeading,cartImg,cartPrice] = await res;
               </h2>
               {/* Pricing */}
               <div className="text-[#151875] text-sm flex justify-center gap-2 ">
-                <span>{item.cartprice}</span>
-                <span className="line-through text-black">
-                  {item.cartprice}
-                </span>
+                <span>${item.cartprice}.00</span>
               </div>
               
             </div>
