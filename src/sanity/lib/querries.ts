102c3202a59842fae9e1040140bb1119ddfa6dfb
SaimaAmjad786 <160956110+SaimaAmjad786@users.noreply.github.com>
2025-01-21 15:07:18 -0800

    updated code

--- src/sanity/lib/querries.ts
+++ src/sanity/lib/querries.ts
@@ -1,4 +1,18 @@
+// import { groq } from "next-sanity";
+
+// export const allProducts = groq `*[_type == "product"]`;
+// export const four = groq `*[_type == "product"] [0..3]`;
+
+
+
 import { groq } from "next-sanity";
 
-export const allProducts = groq `*[_type == "product"]`;
-export const four = groq `*[_type == "product"] [0..3]`;
+// Fetch all products
+export const allProducts = groq`
+  *[_type == "product"]
+`;
+
+// Fetch the first 4 products
+export const fourProducts = groq`
+  *[_type == "product"][0..3]
+`;
--- types/products.ts
+++ types/products.ts
@@ -1,24 +1,4 @@
 
-// export interface Product {
-//     _id:string;
-//     name:string;
-//     type:'product';
-//     image?:{
-//         asset:{
-//             _ref:string;
-//             _type:"image";
-//         }
-//     };
-//     price:number;
-//     description?:string;
-//     discountPercentage:number;
-//     slug:{
-//         _type:"slug";
-//         current:string;
-//     }
-// }
-
-
 export interface Product {
     _id: string;
     name: string;
