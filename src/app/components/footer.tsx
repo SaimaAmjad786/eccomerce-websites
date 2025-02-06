6d3380d1ef35dbfbb4f5d22346e5438d104cbf47
SaimaAmjad786 <160956110+SaimaAmjad786@users.noreply.github.com>
2025-02-06 01:42:18 -0800

    updated code

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -33,11 +33,11 @@ function Footer() {
             />
           </div>
           <div>
-          <Link href="/signup">
+          
           <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
             Sign-Up
           </button>
-          </Link>
+          
           </div>
         </div>
         <p className="text-gray-500 text-sm mt-2 md:text-left text-center">
